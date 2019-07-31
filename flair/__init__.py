0869ae5cca51c5318ad1921cbcb651392b53f6b8
Felicity Wang <cnfxwang@gmail.com>
2019-07-30 21:56:34 -0700

    get freer gpu

--- flair/__init__.py
+++ flair/__init__.py
@@ -1,13 +1,41 @@
 import os
+import subprocess
+from io import StringIO
 
+import numpy as np
+import pandas as pd
 import torch
 
 from .available import get_free_gpus
 
 
 def get_avail_device() -> torch.device:
-    gpu_id = get_free_gpus()[0]  # select the first gpu
+    # gpu_id = get_free_gpus()[0]  # select the first gpu
+    gpu_id = get_freer_gpu()
     return torch.device(f"cuda:{gpu_id}")
+
+
+def get_freer_gpu():
+    # https: // discuss.pytorch.org / t / it - there - anyway - to - let - program - select - free - gpu - automatically / 17560 / 2
+    os.system('nvidia-smi -q -d Memory |grep -A4 GPU|grep Free >tmp')
+    memory_available = [int(x.split()[2]) for x in open('tmp', 'r').readlines()]
+    return np.argmax(memory_available)
+
+
+def get_free_gpu():
+    # https: // discuss.pytorch.org / t / it - there - anyway - to - let - program - select - free - gpu - automatically / 17560 / 2
+    gpu_stats = subprocess.check_output(
+        ["nvidia-smi", "--format=csv", "--query-gpu=memory.used,memory.free"])
+    gpu_df = pd.read_csv(StringIO(u"".join(gpu_stats)),
+                         names=['memory.used', 'memory.free'],
+                         skiprows=1)
+    print('GPU usage:\n{}'.format(gpu_df))
+    gpu_df['memory.free'] = gpu_df['memory.free'].map(
+        lambda x: x.rstrip(' [MiB]'))
+    idx = gpu_df['memory.free'].idxmax()
+    print('Returning GPU{} with {} free MiB'.format(idx, gpu_df.iloc[idx][
+        'memory.free']))
+    return idx
 
 
 # global variable: cache_root
