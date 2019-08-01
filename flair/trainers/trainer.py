12d1e5112c4e2c8eeab04af9ce33df65df7d465f
Felicity Wang <cnfxwang@gmail.com>
2019-08-01 14:21:08 -0700

    flair/

--- flair/trainers/trainer.py
+++ flair/trainers/trainer.py
@@ -71,7 +71,7 @@ class ModelTrainer:
         num_workers: int = 6,
         sampler=None,
         summary_dir: str = None,
-        early_lr_update: bool = True,
+        early_lr_update: bool = None,
         early_lr_start_batch: int = None,
         early_lr_stride_batch: int = None,
         **kwargs,
@@ -275,15 +275,15 @@ class ModelTrainer:
                         # as shuffle is set to True in DataLoader, datapoints
                         # should all have an equal probability to be seen
 
-                        log.info(
-                            f"epoch {epoch + 1} - iter {batch_no}/{total_number_of_batches} - loss "
-                            f"{train_loss / seen_batches:.8f}"
-                        )
-                        iteration = epoch * total_number_of_batches + batch_no
-                        if not param_selection_mode:
-                            weight_extractor.extract_weights(
-                                self.model.state_dict(), iteration
-                            )
+                        # log.info(
+                        #     f"epoch {epoch + 1} - iter {batch_no}/{total_number_of_batches} - loss "
+                        #     f"{train_loss / seen_batches:.8f}"
+                        # )
+                        # iteration = epoch * total_number_of_batches + batch_no
+                        # if not param_selection_mode:
+                        #     weight_extractor.extract_weights(
+                        #         self.model.state_dict(), iteration
+                        #     )
 
                         break
 
