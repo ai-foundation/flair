6e78f774df95e718322db791832456269ab58049
Felicity Wang <cnfxwang@gmail.com>
2019-08-23 01:07:22 -0700

    confusion matrix tag name for new tags

--- flair/models/sequence_tagger_model.py
+++ flair/models/sequence_tagger_model.py
@@ -311,10 +311,18 @@ class SequenceTagger(flair.nn.Model):
             eval_loss /= batch_no
 
             all_labels = list(range(len(self.tag_dictionary.get_items())))
-            if 'S-MICRO_PAUSE' in self.tag_dictionary.get_items():
-                tag_names = ['O', 'S-MICRO_PAUSE', 'S-MINI_PAUSE', 'S-SHORT_PAUSE', 'S-MEDIUM_PAUSE', 'S-LONG_PAUSE']
+            # TODO change names
+            # if 'S-MICRO_PAUSE' in self.tag_dictionary.get_items():
+            #     tag_names = ['O', 'S-MICRO_PAUSE', 'S-MINI_PAUSE', 'S-SHORT_PAUSE', 'S-MEDIUM_PAUSE', 'S-LONG_PAUSE']
+            # else:
+            #     tag_names = ['O', 'S-MINI_PAUSE', 'S-SHORT_PAUSE', 'S-MEDIUM_PAUSE', 'S-LONG_PAUSE']
+            if 'S-5' in self.tag_dictionary.get_items():
+                tag_names = ['O', 'S-1', 'S-2', 'S-3', 'S-4', 'S-5']
+            elif 'S-4' in self.tag_dictionary.get_items():
+                tag_names = ['O', 'S-1', 'S-2', 'S-3', 'S-4']
             else:
-                tag_names = ['O', 'S-MINI_PAUSE', 'S-SHORT_PAUSE', 'S-MEDIUM_PAUSE', 'S-LONG_PAUSE']
+                tag_names = ['O', 'S-1', 'S-2', 'S-3']
+
             ordered_labels = [self.tag_dictionary.get_idx_for_item(i) for i in tag_names]
 
             cm = confusion_matrix(true_labels, pred_labels, all_labels)
@@ -323,9 +331,11 @@ class SequenceTagger(flair.nn.Model):
 
             cm_result = "\nConfusion Matrix\n      "
             for i in ordered_labels:
-                cm_result += "%7s" % self.tag_dictionary.get_item_for_index(i).replace('S-', '').replace('_PAUSE', '')
+                # cm_result += "%7s" % self.tag_dictionary.get_item_for_index(i).replace('S-', '').replace('_PAUSE', '')
+                cm_result += "%7s" % self.tag_dictionary.get_item_for_index(i)
             for i in ordered_labels:
-                cm_result += "\n%6s" % self.tag_dictionary.get_item_for_index(i).replace('S-', '').replace('_PAUSE', '')
+                # cm_result += "\n%6s" % self.tag_dictionary.get_item_for_index(i).replace('S-', '').replace('_PAUSE', '')
+                cm_result += "\n%6s" % self.tag_dictionary.get_item_for_index(i)
                 for j in ordered_labels:
                     cm_result += "%7d" % cm[i][j]
             cm_result += '\n'
